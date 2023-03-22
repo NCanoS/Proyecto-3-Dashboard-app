81d2ea11dd211af6ee8922bac8d4331ded9990f4
emspa07 <empaulin.dwfsc14@bootcamp.ucamp.io>
2023-03-22 14:29:03 -0600

    Se colocaron los estilos globales y del header

---
+++ src/js/delete.js
@@ -0,0 +1,11 @@
+/*const urlBase = ''https://moviesdatabase.p.rapidapi.com/titles/x/titles-by-ids?idsList=tt0001702%2Ctt0001856%2Ctt0001856'
+
+
+const deleteMovies = async () => {
+    const response = await fetch (urlBase + ,{
+        method: "DELETE",
+    })
+    const json = await response.json()
+    console.log(json)
+}
+deleteMovies()*/
